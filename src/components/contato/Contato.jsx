8ad8be382c70a4abbf1cf026ee06b96d6ba68746
Lucas Zamora <113631100+lucaszamorapy@users.noreply.github.com>
2024-02-25 17:43:12 -0300

    fix: contato mobile

--- src/components/contato/Contato.jsx
+++ src/components/contato/Contato.jsx
@@ -7,7 +7,7 @@ const Contato = () => {
       <section id="contato" className="mt-40 bg-blackBirdz py-10 ">
         <div className="container">
           <div className="flex flex-col justify-center items-center gap-10 lg:grid lg:grid-cols-2">
-            <iframe className=" lg:w-[600px] rounded-lg" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14584.73834106728!2d-46.3257902!3d-23.9539121!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce037460477459%3A0xb8d2de822c83c6bd!2sAg%C3%AAncia%20Birdz!5e0!3m2!1spt-BR!2sbr!4v1708887082002!5m2!1spt-BR!2sbr"></iframe>
+            <iframe className=" lg:w-[600px] h-[450px] rounded-lg" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14584.73834106728!2d-46.3257902!3d-23.9539121!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce037460477459%3A0xb8d2de822c83c6bd!2sAg%C3%AAncia%20Birdz!5e0!3m2!1spt-BR!2sbr!4v1708887082002!5m2!1spt-BR!2sbr"></iframe>
             <Forms />
           </div>
         </div>
